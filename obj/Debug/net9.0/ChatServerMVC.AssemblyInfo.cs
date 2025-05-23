889d601e17314ea4475a757ee75623d9d9385ed8
jhmoon2985 <jhmoon2985@gmail.com>
2025-05-23 18:11:06 +0900

    ㅁㅁ

--- obj/Debug/net9.0/ChatServerMVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/ChatServerMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatServerMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0e090bfeb2eb042b946b690f8dc6f25c49aa853")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatServerMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatServerMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
